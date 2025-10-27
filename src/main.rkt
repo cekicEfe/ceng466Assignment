a6153deafd6c6aa49b43d3d74292231c847162e1
cekicEfe <efecekic0@gmail.com>
2025-10-27 14:44:35 +0300

    Newer version

--- src/main.rkt
+++ src/main.rkt
@@ -1,9 +1,26 @@
-#lang typed/racket
+#lang racket
 
-(require math)
-(require racket/dict)
+(define graph (list [list "s" 7 (cons "a" 3) (cons "b" 2)]
+                    [list "a" 6 (cons "d" 4)]
+                    [list "b" 5 (cons "c" 6) (cons "e" 4)]
+                    [list "c" 2 (cons "g" 1)]
+                    [list "d" 6 (cons "f" 5)]
+                    [list "e" 1 (cons "g" 2)]
+                    [list "f" 5 (cons "g" 7)]
+                    [list "g" 0 '()]))
 
-(define (bfs)
-  (array #[0 1 2 3 4 5]) )
+(define (getNode x graph)
+  (let ([maybePair (assoc x graph)])
+    (cond
+      [(pair? maybePair)(cddr maybePair)]
+      [else (error "Does not exist")])))
 
-(bfs)
+;(define (getHeuristicValue x graph)
+;  )
+
+(define (getConnectedNodes x graph)
+  (let ([graph_pair (getNode x graph)])
+    (cddr graph_pair)))
+
+(display (getNode "s" graph))
+;(display (getConnectedNodes "s" graph))
