42fa282ab64c7c7121724399275aa803d005e1f2
cekicEfe <efecekic0@gmail.com>
2025-10-27 19:55:48 +0300

    Newer version

--- src/main.rkt
+++ src/main.rkt
@@ -1,26 +1,47 @@
 #lang racket
 
-(define graph (list [list "s" 7 (cons "a" 3) (cons "b" 2)]
-                    [list "a" 6 (cons "d" 4)]
-                    [list "b" 5 (cons "c" 6) (cons "e" 4)]
-                    [list "c" 2 (cons "g" 1)]
-                    [list "d" 6 (cons "f" 5)]
-                    [list "e" 1 (cons "g" 2)]
-                    [list "f" 5 (cons "g" 7)]
-                    [list "g" 0 '()]))
+(define graph (make-hash
+  (list [cons 's (list [cons 'a 3] [cons 'b 2])]
+        [cons 'a (cons 'd 4)]
+        [cons 'b (list [cons 'c 6] [cons 'e 4])]
+        [cons 'c (cons 'g 1)]
+        [cons 'd (cons 'f 5)]
+        [cons 'e (cons 'g 2)]
+        [cons 'f (cons 'g 7)]
+        [cons 'g '()])))
 
-(define (getNode x graph)
-  (let ([maybePair (assoc x graph)])
+(define heuristic (make-hash
+  (list [cons 's 7]
+        [cons 'a 6]
+        [cons 'b 5]
+        [cons 'c 2]
+        [cons 'd 6]
+        [cons 'e 1]
+        [cons 'f 5]
+        [cons 'g 0])))
+
+(define (getConnectedNodes graph x)
     (cond
-      [(pair? maybePair)(cddr maybePair)]
-      [else (error "Does not exist")])))
+     [(hash-has-key? graph x) (hash-ref graph x)]
+      [else #f]))
 
-;(define (getHeuristicValue x graph)
-;  )
+(define (getHeuristic heuristic x)
+  (cond
+    [(hash-has-key? heuristic x) (hash-ref heuristic x)]
+    [else #f]))
 
-(define (getConnectedNodes x graph)
-  (let ([graph_pair (getNode x graph)])
-    (cddr graph_pair)))
+(define (bfs #:start start
+             #:goal goal
+             #:graph graph)
+  (cond
+    [(and (hash? graph)
+          (hash-has-key? graph start)
+          (hash-has-key? graph goal))
+     (let ([found_nodes    (make-hash)]
+           [visited_nodes (make-hash)])
+      (found_nodes))]
+    [else (error "Invalid args")]))
 
-(display (getNode "s" graph))
-;(display (getConnectedNodes "s" graph))
+(bfs #:graph graph
+     #:start 's
+     #:goal 'g)
